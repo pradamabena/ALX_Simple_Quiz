56910f96d028ad9a0fbfc66bb5e16fabaddf5aca
pradamabena <nonyehm@gmail.com>
2025-01-26 18:12:58 +0200

    Added JavaScript to quiz

---
+++ quiz.js
@@ -0,0 +1,11 @@
+function checkAnswer() {
+  var correctAnswer = "4";
+  var userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+  if (userAnswer === correctAnswer) {
+    document.getElementById("feedback").textContent = "Correct! Well Done.";
+  } else {
+    document.getElementById("feedback").textContent =
+      "That's incorrect. Try Again!";
+  }
+}
+document.getElementById("submit-answer").addEventListener("click", checkAnswer);
